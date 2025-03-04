2ae6acaa6a388831360e821a9e4fd40ece119ff9
Rebillard Baptiste <rebillar@insa-20936>
2025-03-04 14:11:09 +0100

    debug : nouveau C de test

---
+++ test_chiant.c
@@ -0,0 +1,36 @@
+int main(){
+    float x = 0.9;
+    int y = 86;
+    int z = 90e78;
+    int toto, mim = 5;  
+
+    if (x > 0.6){
+        y = 5;
+        printf("LOL");
+    	if (y < 314159216) {
+		x=0.42;
+	} else {
+		x=0.43;
+		while(x<20.0) { x=x+1.0; }
+	}
+    } else {
+        y = 6;
+    }
+
+    int a = 0;
+    while (!a){
+        if (y <= 0){
+            a = 1;
+        } else {
+            y = y - 1;
+        }
+    }
+    printf("Ouais");
+    int x = add(5, 6);
+
+    return 0;
+}
+
+int add(int a, int b){
+    return a + b;
+}
