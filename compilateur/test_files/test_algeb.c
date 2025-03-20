16465e1a48a32da3229e70fe71dfdf04c1783333
Baptiste <baptiste.rebillard@outlook.fr>
2025-03-20 23:57:23 +0100

    chore: clear

--- compilateur/test_files/test_algeb.c
+++ compilateur/test_files/test_algeb.c
@@ -1,15 +1,15 @@
 int main(){
-int a = 7;
-if (a>=2){
-    int a = 8+2*3;
-    int b = 1;
-}else {
-    while (a<2){
+    int a = 7;
+    if (a>=2){
         int a = 8+2*3;
         int b = 1;
+    } else {
+        while (a<2){
+            int a = 8+2*3;
+            int b = 1;
+        }
+        int a = 8+2*3;
+        int b = 1;
+        
     }
-    int a = 8+2*3;
-    int b = 1;
-    
-}
 }
