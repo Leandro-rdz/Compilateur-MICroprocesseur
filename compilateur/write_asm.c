9bfd3ea1650c328f6b3dd9e3ddb7b3db8ecdd2d3
Baptiste <baptiste.rebillard@outlook.fr>
2025-03-20 09:25:44 +0100

    feat : ajouts d'instructions

--- compilateur/write_asm.c
+++ compilateur/write_asm.c
@@ -48,6 +48,27 @@ void ASM(enum OpCode op, int a, int b, int c) {
 		case STORE:
 			fprintf(file, "STORE %d %d\n",a,b);
 			break;
+		case EQU: // a=1 si b=c sinon a=0
+			fprintf(file, "EQU %d %d %d\n", a, b, c);
+			break;
+		case INF: // a=1 si b<c sinon a=0
+			fprintf(file, "INF %d %d %d\n", a, b, c);
+			break;
+		case INFE: // a=1 si b<=c sinon a=0
+			fprintf(file, "INFE %d %d %d\n", a, b, c);
+			break;
+		case SUP: // a=1 si b>c sinon a=0
+			fprintf(file, "SUP %d %d %d\n", a, b, c);
+			break;
+		case SUPE: // a=1 si b>=c sinon a=0
+			fprintf(file, "SUPE %d %d %d\n", a, b, c);
+			break;
+		case JMP: // saut à l'@ de a
+			fprintf(file, "JMPPC %d %d\n", a, 1);
+			break;
+		case JMPC: // saut à l'@ de a si b !=0
+			fprintf(file, "JMPC %d %d\n", a, b);
+			break;
 	}
     fflush(file);
 }
