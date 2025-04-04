15a07c6bd3dca472e9dd8b324f761cbbb51185f6
Leandro-rdz <124134376+Leandro-rdz@users.noreply.github.com>
2025-04-04 11:12:07 +0200

    add to registers

--- Processeur/Processeur.srcs/sources_1/new/Registers.vhd
+++ Processeur/Processeur.srcs/sources_1/new/Registers.vhd
@@ -2,26 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity ALU is
+entity registers is
     Port(
         ADA   : in  std_logic_vector(3 downto 0);
         ADB   : in  std_logic_vector(3 downto 0);
         ADW   : in  std_logic_vector(3 downto 0);
-        S   : out std_logic_vector(7 downto 0);
-        SEL : in  std_logic_vector(2 downto 0);
-        CAR : out std_logic;
-        OVF : out std_logic;
-        NEG : out std_logic;
-        NUL : out std_logic
+        W     : in  std_logic;
+        Data  : in  std_logic_vector(7 downto 0);
+        RST   : in  std_logic;
+        CLK   : in  std_logic;
+        QA    : out std_logic_vector(7 downto 0);
+        QB    : out std_logic_vector(7 downto 0) 
     );
-end ALU;
+end registers;
 
-ARCHITECTURE Behavior OF ALU IS
-    SIGNAL result : std_logic_vector(8 DOWNTO 0); 
-    SIGNAL car_out : std_logic;
-    SIGNAL ovf_out : std_logic;
-    SIGNAL neg_out : std_logic;
-    SIGNAL nul_out : std_logic;
+ARCHITECTURE Behavior OF registers IS
+
 BEGIN
 
 END Behavior;
