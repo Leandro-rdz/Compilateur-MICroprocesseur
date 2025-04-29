dfc2cffb50590b57e94fdd4a362a6c17ccb29e84
Rebillard Baptiste <rebillar@insa-11282>
2025-04-29 17:39:19 +0200

    feat(not working yet) : adding the first version of the pipeline

---
+++ Processeur/Processeur.srcs/sources_1/new/interface.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity interface_element is
+    Port (
+        a_in  : in  std_logic_vector(7 downto 0);
+        b_in  : in  std_logic_vector(7 downto 0);
+        c_in  : in  std_logic_vector(7 downto 0);
+        op_in : in  std_logic_vector(3 downto 0);
+
+        a_out  : out std_logic_vector(7 downto 0);
+        b_out  : out std_logic_vector(7 downto 0);
+        c_out  : out std_logic_vector(7 downto 0);
+        op_out : out std_logic_vector(3 downto 0)
+    );
+end interface_element;
+
+architecture Behavioral of interface_element is
+begin
+    a_out  <= a_in;
+    b_out  <= b_in;
+    c_out  <= c_in;
+    op_out <= op_in;
+end Behavioral;
--- Processeur/Processeur.srcs/sources_1/new/processor.vhd
+++ Processeur/Processeur.srcs/sources_1/new/processor.vhd
@@ -3,59 +3,133 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity processor is
-    Port (
-        ADA  : in std_logic_vector(3 downto 0);  -- Adresse lecture A
-        ADB  : in std_logic_vector(3 downto 0);  -- Adresse lecture B
-        ADW  : in std_logic_vector(3 downto 0);  -- Adresse écriture
-        W    : in std_logic;                    -- Signal d'écriture
-        RST  : in std_logic;                    -- Reset
-        CLK  : in std_logic;                    -- Horloge
-        SEL  : in std_logic_vector(2 downto 0); -- Sélection d'opération ALU
-        S    : out std_logic_vector(7 downto 0);-- Résultat ALU
-        CAR  : out std_logic;
-        OVF  : out std_logic;
-        NEG  : out std_logic;
-        NUL  : out std_logic
-    );
 end processor;
 
 architecture Behavioral of processor is
 
     -- Signaux internes pour liaison entre les modules
-    signal QA, QB, QC : std_logic_vector(7 downto 0);
-    signal OP : std_logic_vector(3 downto 0);
-    signal result : std_logic_vector(7 downto 0);
-
+    signal instruction_selected : std_logic_vector(31 downto 0) := (others => '0');                                 -- post fetch
+    signal A1, B1, C1, OP1 : std_logic_vector(7 downto 0);                                                          -- post decode
+    signal A2, B2_select_in, B2_select_out, B2_mux, C2_in, C2_out, OP2 : std_logic_vector(7 downto 0);              -- banc de registre
+    signal A3, B3_alu_in, B3_alu_out, B3_mux, C3, OP3 : std_logic_vector(7 downto 0);                               -- UAL
+    signal A4, B4_in, B4_mux1, B4_out, B4_mux2, OP4 : std_logic_vector(7 downto 0);                                 -- Memoire des données
+    signal A5, B5, OP5 : std_logic_vector(7 downto 0);                                                              -- Avant écriture dans les bancs de registres 
+        
+    -- signaux ignorés pour l'instant genre les FLAG de l'ALU on s'en fiche pour l'instant 
+    signal IGNORED_1, IGNORED_6, IGNORED_7 : std_logic_vector(7 downto 0) := (others => '0');
+    signal IGNORED_2, IGNORED_3, IGNORED_4, IGNORED_5 : std_logic := '0';
 begin
+    -- Instanciation de la mémoire des instructions
+    instruction_memory_inst : entity work.Instr_Memory
+        port map (
+            Addr        =>  (others => '0'),    -- TODO
+            CLK         =>  '0',    -- TODO
+            Instr_Out   =>  instruction_selected
+        );
+        
+    -- Instanciation du décoder d'instruction
+    instruction_decoder_inst : entity work.Instruction_decoder
+        port map (
+          Instruction => instruction_selected,
+          A => A1,
+          B => B1,
+          C => C1,
+          OP => OP1
+        );
+        
+    -- Instanciation de LI/DI
+    li_di_inst : entity work.interface_element
+        port map (
+            a_in   => A1,
+            b_in   => B1,
+            c_in   => C1,
+            op_in  => OP1,
+            a_out  => A2,
+            b_out  => B2_select_in,
+            c_out  => C2_in,
+            op_out => OP2
+        );
 
     -- Instanciation du banc de registres
     regfile_inst : entity work.registers
         port map (
-            ADA   => ADA,
-            ADB   => ADB,
-            ADW   => ADW,
-            W     => W,
-            Data  => result,  -- Résultat ALU à écrire
-            RST   => RST,
-            CLK   => CLK,
-            QA    => QA,
-            QB    => QB
+            ADA   => B2_select_in,
+            ADB   => C2_in,
+            ADW   => A5,
+            W     => '0', -- TODO : donner OP5 si ça correspond bien au OPCODE d'écriture dans les registres
+            Data  => B5, -- TODO
+            RST   => '0', -- TODO
+            CLK   => '0', -- TODO
+            QA    => B2_select_out,
+            QB    => C2_out
         );
+       
+        
+    -- Instanciation de DI/EX
+    di_ex_inst : entity work.interface_element
+        port map (
+            a_in   => A2,
+            b_in   => B2_mux, -- TODO on a B2_select_out ou B2_select_in à donner à manger ici (b_in) selon le OP2 
+            c_in   => C2_out,
+            op_in  => OP2,
+            a_out  => A3,
+            b_out  => B3_alu_in,
+            c_out  => C3,
+            op_out => OP3
+        );
+        
 
     -- Instanciation de l'ALU
     alu_inst : entity work.ALU
         port map (
-            A   => QA,
-            B   => QB,
-            S   => result, -- Résultat à écrire et à renvoyer
-            SEL => SEL,
-            CAR => CAR,
-            OVF => OVF,
-            NEG => NEG,
-            NUL => NUL
+            A   => B3_alu_in,
+            B   => C3,
+            S   => B3_alu_out,
+            SEL => IGNORED_1, -- FLAG IGNORED
+            CAR => IGNORED_2, -- FLAG IGNORED
+            OVF => IGNORED_3, -- FLAG IGNORED
+            NEG => IGNORED_4, -- FLAG IGNORED
+            NUL => IGNORED_5  -- FLAG IGNORED
         );
-
-    -- Propagation du résultat vers l'extérieur
-    S <= result;
+        
+    -- Instanciation de EX/MEM
+    ex_mem_inst : entity work.interface_element
+        port map (
+            a_in   => A3,
+            b_in   => B3_mux, -- TODO on a B3_alu_in ou B3_alu_out à donner à manger selon le OP3
+            c_in   => (others => '0'),
+            op_in  => OP3,
+            a_out  => A4,
+            b_out  => B4_in,
+            c_out  => IGNORED_6,
+            op_out => OP4
+        );
+        
+    memory_inst : entity work.Data_Memory
+        port map (
+        Addr     => B4_mux1,
+        Data_In  => B4_mux1, -- TODO on doit donner à mux1 soit B4_in soit A4 selon la situation
+        RW       => '0',     -- donner OP4 que si ça correspond au bon OPCODE
+        RST      => '0',
+        CLK      => '0',
+        Data_Out => B4_out
+        );
+    
+    -- Instanciation de MEM/RE
+    mem_re_inst : entity work.interface_element
+        port map (
+            a_in   => A4,
+            b_in   => B4_mux2, -- TODO on a B4_in ou B4_out donner à manger selon le OP4
+            c_in   => (others => '0'),
+            op_in  => OP3,
+            a_out  => A5,
+            b_out  => B5,
+            c_out  => IGNORED_7,
+            op_out => OP5
+        );
+        
+    --stim_proc: process
+    --begin
+    --end process;
 
 end Behavioral;
