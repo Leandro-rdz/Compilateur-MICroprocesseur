9dfc8977a123fb4cb4bcc3ab587f7f8e313726f5
Baptiste <baptiste.rebillard@outlook.fr>
2025-05-21 08:40:38 +0200

    feat : debug

--- Processeur/sim/Processor_global_tb.vhd
+++ Processeur/sim/Processor_global_tb.vhd
@@ -8,7 +8,9 @@ architecture Behavioral of Test is
 
     signal CLK : std_logic := '0';
 
-    signal contr_ld1, contr_ld2, contr_itr1, contr_itr2 : std_logic_vector(7 downto 0) := "00000010";
+    signal contr_ld1, contr_ld2 :std_logic_vector(7 downto 0) := "00000010";
+    signal contr_itr1 : std_logic_vector(7 downto 0) := "00001010"; --10 donc 0xA
+    signal contr_itr2 : std_logic_vector(7 downto 0) := "00000110"; -- 0x6
     signal contr_btn : std_logic_vector(4 downto 0) := (others => '0');
 
     -- Instance du processeur
